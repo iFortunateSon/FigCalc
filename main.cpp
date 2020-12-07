7436f52f5b0dbfed2e2c0e00ea3b235954a6f9ed
iFortunateSon <itbestik@gmail.com>
2020-12-07 19:35:36 +0300

    ada

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    //penis;
-    // fdffssssss
+    // asdasdasdasdasdasdasdasd------>>>>
     return a.exec();
 }
